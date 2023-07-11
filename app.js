ff743028cb3c92fbc053bc51647664839910c9ce
ahmedehabb2 <ahmed.ehab.mansour@gmail.com>
2023-07-11 13:33:34 +0300

    build APIs

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const fs = require('fs');
-
 const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
+    );
+    
+app.use(express.json());
 
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
     res.status(200).json({
         status: 'success',
         results: tours.length,
@@ -13,8 +15,74 @@ app.get('/api/v1/tours', (req, res) => {
             tours
         }
     });
-});
+};
 
+const getTour = (req, res) => {
+    console.log(req.params);
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id);
+
+    // if (id > tours.length) {
+    if (!tour) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour
+        }
+    });
+};
+
+const createTour = (req, res) => {
+    console.log(req.body);
+    res.send('Done');
+};
+
+const updateTour = (req, res) => {
+    if (req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });        
+    }
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: '<Updated tour here...>'
+        }
+    });
+};
+
+const deleteTour = (req, res) => {
+    if (req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });        
+    }
+
+    res.status(204).json({
+        status: 'success',
+        data: null
+    });
+};
+
+
+
+app.route('/api/v1/tours')
+    .get(getAllTours)
+    .post(createTour);
+
+app.route('/api/v1/tours/:id')
+    .get(getTour)
+    .patch(updateTour)
+    .delete(deleteTour);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
