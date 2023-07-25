49d642066cf2c01eca16ad767271241ac3f3881b
ahmedehabb2 <ahmed.ehab.mansour@gmail.com>
2023-07-25 23:19:33 +0300

    link mongoDB

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const app = require('./app');
+const mongoose = require('mongoose')
 
+mongoose.connect(process.env.DATABASE,{
+    useNewUrlParser : true,
+    useCreateIndex : true,
+    useFindAndModify : false
+}).then(con => {
+    console.log('DB connection successful')
+})
 
 app.listen(process.env.port, () => {
     console.log(`Server is running on port ${process.env.port}`);
