d793373029f78cda931b7265036f98844a38874b
ahmedehabb2 <ahmed.ehab.mansour@gmail.com>
2023-07-27 19:03:13 +0300

    create tour model

--- server.js
+++ server.js
@@ -4,11 +4,30 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE,{
     useNewUrlParser : true,
     useCreateIndex : true,
-    useFindAndModify : false
-}).then(con => {
+    useFindAndModify : false,
+    useUnifiedTopology: true
+}).then((con) => {
     console.log('DB connection successful')
-})
+}).catch(err => console.log(err))
 
-app.listen(process.env.port, () => {
-    console.log(`Server is running on port ${process.env.port}`);
+const tourSchema = new mongoose.Schema({
+    name:{
+        type : String,
+        required : [true, 'A tour must have a name'],
+        unique:true
+    },
+    rating:{
+        type:Number,
+        default:4.5
+    },
+    price:{
+        type : Number,
+        required : [true,'A tour must have a price']
+    }
+});
+
+const Tour = mongoose.model('Tour',tourSchema);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
 });
