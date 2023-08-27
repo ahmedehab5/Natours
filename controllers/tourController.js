a6bdc322f67b0b93ce666518bf8798918260abac
ahmedehabb2 <ahmed.ehab.mansour@gmail.com>
2023-08-27 17:17:45 +0300

    link tours APIs with mongoose

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,77 +1,93 @@
 const Tour = require('./../models/tourModel');
 
-exports.checkBody = (req , res, next) => {
-    if(!req.body.price || !req.body.name){
-        return res.status(400).JSON({
-            status:'faild',
-            message:'missing properties'
+
+exports.getAllTours = async (req, res) => {
+    try{
+        const tours =  await Tour.find()
+        res.status(200).json({
+            status: 'success',
+            results: tours.length,
+            data: {
+                tours
+            }
         });
     }
-    next();
+    catch(err) {
+        res.status(404).json({
+            status : 'failed',
+            message : err
+        })
+    }
 };
 
-exports.getAllTours = (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        results: tours.length,
-        data: {
-            tours
-        }
-    });
+exports.getTour = async (req, res) => {
+    try{
+        const tour = await Tour.findById(req.params.id)
+        res.status(200).json({
+            status : 'success',
+            data : {
+                tour
+            }
+        })
+    }
+    catch(err){
+        res.status(404).json({
+            status : 'failes',
+            message : err
+        })
+    }
 };
 
-exports.getTour = (req, res) => {
-    console.log(req.params);
-    const id = req.params.id * 1;
-    const tour = tours.find(el => el.id === id);
+exports.createTour = async (req, res) => {
+    try{
+        const newTour = await Tour.create(req.body);
 
-    // if (id > tours.length) {
-    if (!tour) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
+        res.status(201).json({
+            status : "success",
+            data : newTour
+        })
+    }
+    catch(err){
+        res.status(400).json({
+            status : 'fail',
+            message : err
+        })
+    }
+};
+
+exports.updateTour = async (req, res) => {
+    try{
+        const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+            new : true,
+            runValidators : true
+        })
+        res.status(200).json({
+            status : 'success',
+            data :{
+                tour
+            }
+        })
+    }
+    catch(err) {
+        res.status(404).json({
+            status : 'failed',
+            message : err
+        })
+    }
+};
+
+exports.deleteTour = async (req, res) => {
+    try{
+        await Tour.findByIdAndDelete(req.params.id)
+        res.status(204).json({
+            status: 'success',
+            data: null
         });
     }
-
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour
-        }
-    });
-};
-
-exports.createTour = (req, res) => {
-    console.log(req.body);
-    res.send('Done');
-};
-
-exports.updateTour = (req, res) => {
-    if (req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });        
+    catch(err) {
+        res.status(404).json({
+            status : 'failed',
+            message : err
+        })
     }
-
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour: '<Updated tour here...>'
-        }
-    });
-};
-
-exports.deleteTour = (req, res) => {
-    if (req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });        
-    }
-
-    res.status(204).json({
-        status: 'success',
-        data: null
-    });
 };
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const app = require('./app');
+const mongoose = require("mongoose") 
 
 mongoose.connect(process.env.DATABASE,{
     useNewUrlParser : true,
